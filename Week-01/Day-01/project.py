0112ed52829c8920f60b48e8f3ce562b435bd2f3
Rishabh Jain <rishabhjain7458@gmail.com>
2026-07-15 00:49:30 +0530

    Python code added

---
+++ Week-01/Day-01/project.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+role = input("Enter your role: ")
+technology = input("Favourite technology: ")
+salary = input("Dream monthly salary: ")
+
+print("\n----------")
+
+print("Hello", name)
+print("Age:", age)
+print("Role:", role)
+print("Favourite Technology:", technology)
+print("Dream Salary:", salary)
+
+print("\nLet's become an AI Automation Engineer! 🚀")
